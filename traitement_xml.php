ff59560c5f7ccd86e5be037249244210363d311c
emySchoettel <56391268+emySchoettel@users.noreply.github.com>
2021-03-16 16:47:58 +0100

    hhhh

---
+++ traitement_xml.php
@@ -0,0 +1,19 @@
+<?php
+    $tab = array(
+        array(
+            'id' => 1,
+            'nom' => 'schoettel',
+            'prenom' => 'emy',
+            'ville' => 'strasbourg'
+        ),
+        array(
+            'id' => 2,
+            'nom' => "schoettel",
+            'prenom' => 'luc',
+            'ville' => "colmar"
+        )
+    );  
+
+    var_dump($tab);
+    echo ($tab);
+?>
