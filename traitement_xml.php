c94d25b06f302cf274c12bab0df599e9dec2bc9a
emySchoettel <56391268+emySchoettel@users.noreply.github.com>
2021-03-16 16:56:22 +0100

    dddd

--- traitement_xml.php
+++ traitement_xml.php
@@ -1,19 +1,22 @@
 <?php
-    $tab = array(
-        array(
-            'id' => 1,
-            'nom' => 'schoettel',
-            'prenom' => 'emy',
-            'ville' => 'strasbourg'
-        ),
-        array(
-            'id' => 2,
-            'nom' => "schoettel",
-            'prenom' => 'luc',
-            'ville' => "colmar"
-        )
-    );  
+header('Content-type: text/xml');
+$tab = array(
+    array(
+        'id' => 1,
+        'nom' => 'schoettel',
+        'prenom' => 'emy',
+        'ville' => 'strasbourg'
+    ),
+    array(
+        'id' => 2,
+        'nom' => "schoettel",
+        'prenom' => 'luc',
+        'ville' => "colmar"
+    )
+);  
 
-    var_dump($tab);
-    echo ($tab);
-?>
+var_dump($tab);
+echo ($tab);
+?>    
+
+    
